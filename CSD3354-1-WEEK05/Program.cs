c2a437087b1648835aa4eb262b3ce6991c99d55f
visualstudio <visualstudio@122-PC10>
2019-02-08 20:40:27 -0500

    cchgcchchg

--- CSD3354-1-WEEK05/Program.cs
+++ CSD3354-1-WEEK05/Program.cs
@@ -4,12 +4,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSD3354_1_WEEK05
+namespace CSD3354_1_WEEK05()
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Elevator e = new Elevator();
+            e.setup();
+            e.TraverseList();
+        }
+    }
+
+    class Node
+    {
+        public Node() { }
+        public Node elevatorUp;
+        public string FloorNumber;
+    }
+
+    class Department
+    {
+        public Department aDepartment;
+
+    }
+
+    class Elevator
+    {
+        Node Head;
+        Node FirstFloor;
+        Node SecondFloor;
+        Node ThirdFloor;
+        Node FourthFloor;
+
+        public void setup()
+        {
+            FirstFloor = new Node();
+            SecondFloor = new Node();
+            ThirdFloor = new Node();
+            FourthFloor = new Node();
+            Head = FirstFloor;
+            FirstFloor.FloorNumber = "First Floor";
+            FirstFloor.elevatorUp = SecondFloor;
+            SecondFloor.FloorNumber = "Second Floor";
+            SecondFloor.elevatorUp = ThirdFloor;
+            ThirdFloor.FloorNumber = "Third Floor";
+            ThirdFloor.elevatorUp = FourthFloor;
+            FourthFloor.FloorNumber = "Fourth Floor";
+            FourthFloor.elevatorUp = null;
+        }
+
+        public void TraverseList()
+        {
+            Node temp;
+            temp = Head;
+
+            // where am I going to start?
+
+            while (temp != null)
+            {
+                Console.WriteLine(temp.FloorNumber);
+                temp = temp.elevatorUp;
+
+            }
+
         }
     }
 }
